069582f0f3ac2d57784241ea3f9c0ced1e3e652b
Fuzail <fuzailgilani@gmail.com>
2018-06-30 15:24:33 -0500

    part 1 of assignment now opens file passed as argument, and prints ascii value of each byte

--- part1/assignment3part1.c
+++ part1/assignment3part1.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char * argv[]) {
+
+	FILE * fp = fopen(argv[1], "r");
+	int c;
+
+	while(1) {
+      c = fgetc(fp);
+      if( feof(fp) ) {
+         break ;
+      }
+      printf("%d\n", c);
+  }
+
+	fclose(fp);
+
 	return 0;
 }
-
