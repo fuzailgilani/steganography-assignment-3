725ef71e8f53e6c54bf8c4f8fc47f7535958ff4c
Fuzail <fuzailgilani@gmail.com>
2018-06-30 18:22:18 -0500

    added documentation for part 1, updated README, and included compiled part 1

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# Steganography Assignment 3
+# Steganography Assignment 3 #
 
-Part 1: Calculate entropy of a file.
+## Part 1: Calculate entropy of a file. ##
 
-Part 2: Hide a message in a text file, using LSB steganography.
+Completed functionality. To compile code, run the following command in the directory that contains the .c file:
+`gcc -o entropy assignment3part1.c -lm`
+
+To calculate the entropy of a file after compiling, use the following command:
+`./entropy [FILE_NAME]`
+where the `[FILE_NAME]` is the name of the file you want to calculate the entropy of.
+
+The `-lm` flag is to link the mathematics library from C. This is required to use the math.h file in C.
+
+## Part 2: Hide a message in a text file, using LSB steganography. ##
+
+TODO: Begin part 2.
--- part1/assignment3part1.c
+++ part1/assignment3part1.c
@@ -2,50 +2,61 @@
 #include <stdlib.h>
 #include <math.h>
 
-double log_base_2(double num);
+double log_base_2(double num); // function prototype for log_base_2() function
 
 int main(int argc, char * argv[]) {
 
-	FILE * fp = fopen(argv[1], "r");
-	int c;
-	int sumOfUniqueChar = 0;
-	int totalNumOfCharacters = 0;
-	int asciiArr[256] = {0};
-	double entropy = 0;
+	FILE * fp = fopen(argv[1], "rb"); // open the file in binary read mode
 
-	while(1) {
-      c = fgetc(fp);
-      if( feof(fp) ) {
-         break;
+	int c; // variable used to store each character as it is read from file
+	int sumOfUniqueChar = 0; // variable to store sum of the total unique characters in the file
+	int lengthOfFile = 0; // variable to store total number of characters the file contains
+	int asciiArr[256] = {0}; // array where each index corresponds to an ASCII code for a character. The element stored in each index is the number of times that character appears in the file
+	double entropy = 0; // variable to store entropy as it is calculated
+	double ratio = 0; // variable to store ratio of actual entropy to maximum entropy as it is calculated
+
+	while(1) { // keep looping
+      c = fgetc(fp); // retrieve on character at a time from file
+      if( feof(fp) ) { // if end of file,
+         break; //        	break loop
       }
 
-			asciiArr[c]++;
-			totalNumOfCharacters++;
-			if(asciiArr[c] == 1) {
-				sumOfUniqueChar++;
+			asciiArr[c]++; // increment element of array where index == ASCII representation of current character
+			lengthOfFile++; // increment counter variable that tracks number of characters in file
+			if(asciiArr[c] == 1) { // if current character has been encountered for the first time
+				sumOfUniqueChar++; //			increment counter variable that tracks number of unique characters in file
 			}
   }
 
-	printf("Total number of unique characters: %d\n", sumOfUniqueChar);
+	printf("Total number of unique characters: %d\n", sumOfUniqueChar); // print number of unique characters
 
-	for(int i = 0; i < 256; i++){
-		double probabilityOfChar = (double)asciiArr[i] / (double)totalNumOfCharacters;
+	for(int i = 0; i < 256; i++){ // loop through array containing total of each ASCII character
+		double probabilityOfChar = (double)asciiArr[i] / (double)lengthOfFile; // calculate probability of character within string; total times it appears divided by total length of file
 
-		if (asciiArr[i] != 0) {
-			entropy += probabilityOfChar * log_base_2(1 / probabilityOfChar);
+		if (asciiArr[i] != 0) { // if the character appears in the file,
+			entropy += probabilityOfChar * log_base_2(1 / probabilityOfChar); // then calculate the entropy it contributes, and increment total entropy by that amount
 		}
 	}
 
-	double ratio = entropy / 8;
+	ratio = entropy / 8; // calculate ratio of actual entropy to maximum; 256 possible characters means there is a maximum of 8 bits of entropy
 
-	printf("Entropy: %lf\n", entropy);
+	printf("Entropy: %lf\n", entropy); // display entropy and ratio
 	printf("Ratio: %lf\n", ratio);
 
-	fclose(fp);
+	fclose(fp); // close file
 
-	return 0;
+	return 0; // end function
 }
 
+
+/*
+** double log_base_2(double num)
+** Calculates log base 2 of argument
+** Parameters:
+** 	num - double - number to calculate log base 2 of
+** Returns:
+**  log(num) / log(2) - calculates log base 2 using log change of base formula
+*/
 double log_base_2(double num) {
 	return log(num) / log(2);
 }
